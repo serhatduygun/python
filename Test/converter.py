6ff0ec54e2aa8e3846a3293964ad29217da7ec15
[Scot S. Duygun] <[serhatduygun@gmail.com]>
2020-06-06 09:41:42 -0400

    [Setup for the ubuntu server]

---
+++ Test/converter.py
@@ -0,0 +1,5 @@
+# ask user enter a distance in kilometers as integar
+distance_kilo = int(input("Pleas enter a distance in kilometers: "))
+distance_mile = distance_kilo*0.6214
+
+print(distance_kilo, ' kilometre is ',distance_mile, 'Mile')
